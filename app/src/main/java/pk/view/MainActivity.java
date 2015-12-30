7c133c76defc72ff11292c77cce0145f8cbf7760
zijiao <zijiao@mogujie.com>
2015-12-30 09:57:36 +0800

    fix

--- app/src/main/java/pk/view/MainActivity.java
+++ app/src/main/java/pk/view/MainActivity.java
@@ -2,21 +2,29 @@ package pk.view;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        initView();
+    }
 
+    private void initView() {
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        button.setText("perfect");
     }
 
 }
